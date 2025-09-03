10396f28e283cfb2079853d0384f5395cceab5b8
leandrabelva <leandrabelva26@gmail.com>
2025-09-04 04:38:45 +0700

    Ubah index.php

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
-    require $maintenance;
+if (file_exists(_DIR_.'/storage/framework/maintenance.php')) {
+    require _DIR_.'/storage/framework/maintenance.php';
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+require _DIR_.'/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once _DIR_.'/bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
